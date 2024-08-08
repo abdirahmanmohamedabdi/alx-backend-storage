5f1f1b63a46c50fa40222f9de4a207317af0d53e
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-08-08 13:46:52 +0300

    new

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,6 @@
+-- Lists all bands with Glam rock as their main styel, ranked by their lifespan.
+-- SELECT band-name, (ifnull(split, 2022) - formed) AS lifespan
 SELECT band_name, (IFNULL(split, 2022) -formed) AS lifespan
-FROM metal_bands
-WHERE style LIKE '%Glam Rock%'
-ORDER BY lifespan DESC;
+    FROM metal_bands
+    WHERE style LIKE '%Glam Rock%'
+    ORDER BY lifespan DESC;
--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,3 +1,5 @@
+-- creates a trigger that decreases the quantity
+-- of an item in the items table when an order is placed
 DROP TRIGGER IF EXISTS reduce_quantity;
 DELIMITER $$
 CREATE TRIGGER reduce_quantity
@@ -5,7 +7,7 @@ AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
     UPDATE items
-    SET quantity = quantity - NEW.number
-    WHERE name = NEW.item_name;
+        SET quantity = quantity - NEW.number
+        WHERE name = NEW.item_name;
 END $$
 DELIMITER ;
