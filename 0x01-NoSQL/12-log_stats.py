71f3eeae9b881f446bdced522bb4f48c7d1ac417
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-08-13 12:33:16 +0300

    new

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -17,7 +17,8 @@ if __name__ == "__main__":
     delete_num = nginx_logs.count_documents({"method": "DELETE"})
     get_status = nginx_logs.count_documents({
         "method": "GET",
-        "path": "/status"})
+        "path": "/status"
+    })
 
     print("{} logs".format(docs_num))
     print("Methods:")
